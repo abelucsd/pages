323a20c03262d5a49d4b6156a1570fdc2101ca56
Angelo <belmonte.amp@gmail.com>
2022-07-25 12:09:22 -0400

    Cards.js skeleton

---
+++ src/components/Cards.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import Card from './Card'
+
+function Cards(props) {
+    const [numCards, setNumCards] = useState(0)
+    const [cardArray, setCardArray] = useState([])
+
+    useEffect(() => {
+
+    }, [])
+
+  return (
+    <div className='cards'>
+
+    </div>
+  )
+}
+
+export default Cards
